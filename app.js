873710a24861b350c4bc29efc6d096c4ba6f10ee
rafi007akhtar <alimdrafi@gmail.com>
2019-07-15 23:36:58 +0530

    Ajax fetching and parsing complete

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ submit.addEventListener("click", function() {
     
     if (name == myName && pass == myPass) {
         alert("Login successful");
+        // URL: https://api.myjson.com/bins/yangz
+        $.ajax({
+            url:"https://api.myjson.com/bins/yangz",
+            type:"GET",
+            data:'{"key":"value"}',
+            contentType:"application/json; charset=utf-8",
+            dataType:"json",
+            success: function(data, textStatus, jqXHR){
+//                console.log(data);
+                
+                let display = document.querySelector("#display");
+                let details = document.querySelector("#details");
+                details.style.visibility = "visible";
+                for (let key in data) {
+                    display.innerHTML = `${display.innerHTML} <li> ${key.toUpperCase()}: ${data[key]} </li>`
+                }
+            }
+        }); 
     }
     else {
         alert("Login failed");
